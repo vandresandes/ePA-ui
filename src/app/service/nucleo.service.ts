bb38188ac11ddf5f0371a4a81443e78e03ca45e1
vandresandes <vandresandes@gmail.com>
2019-01-03 11:40:49 -0300

    Criação dos métodos do crud

--- src/app/service/nucleo.service.ts
+++ src/app/service/nucleo.service.ts
@@ -1,28 +1,38 @@
 import { Nucleo } from './../model/nucleo';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NucleoService {
-  url: string = "http://localhost:8080/nucleo";
+
+  resource: string = "nucleo";
 
   constructor(private httpClient: HttpClient) { }
 
   findAll() {
-    return this.httpClient.get(this.url).pipe(res=> res);
+    return this.httpClient.get(`${environment.apiUrl}/${this.resource}`).pipe();
   }
 
   save(entity: Nucleo) {
-    // post
+    return this.httpClient.post(`${environment.apiUrl}/${this.resource}`, entity, { observe: 'response' }).pipe();
   }
 
   deleteById(id: number) {
-    return this.httpClient.delete(`${this.url}/id`).pipe(res=> res);
+    return this.httpClient.delete(`${environment.apiUrl}/${this.resource}/${id}`).pipe();
   }
 
   findById(id: number) {
-    return this.httpClient.get(`${this.url}/id`).pipe(res=> res);
+    return this.httpClient.get(`${environment.apiUrl}/${this.resource}/${id}`).pipe(res=> res);
+  }
+
+  pesquisar(nome: string) {
+    if (nome === null) {
+      return this.httpClient.get(`${environment.apiUrl}/${this.resource}/buscar/`).pipe(res=> res);
+    } else {
+      return this.httpClient.get(`${environment.apiUrl}/${this.resource}/buscar/${nome}`).pipe(res=> res);
+    }
   }
 }
--- src/app/service/termo-geral.service.ts
+++ src/app/service/termo-geral.service.ts
@@ -1,9 +1,37 @@
+import { TermoGeral } from './../model/termoGeral';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TermoGeralService {
+  resource: string = "termogeral";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  findAll() {
+    return this.httpClient.get(`${environment.apiUrl}/${this.resource}`).pipe();
+  }
+
+  save(entity: TermoGeral) {
+    return this.httpClient.post(`${environment.apiUrl}/${this.resource}`, entity, { observe: 'response' }).pipe();
+  }
+
+  deleteById(id: number) {
+    return this.httpClient.delete(`${environment.apiUrl}/${this.resource}/${id}`).pipe();
+  }
+
+  findById(id: number) {
+    return this.httpClient.get(`${environment.apiUrl}/${this.resource}/${id}`).pipe(res=> res);
+  }
+
+  pesquisar(nome: string) {
+    if (nome === null) {
+      return this.httpClient.get(`${environment.apiUrl}/${this.resource}/buscar/`).pipe(res=> res);
+    } else {
+      return this.httpClient.get(`${environment.apiUrl}/${this.resource}/buscar/${nome}`).pipe(res=> res);
+    }
+  }
 }
