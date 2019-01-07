10f0864ff9dce28a4d7d403e8806e7364aca1eb5
vandresandes <vandresandes@gmail.com>
2019-01-07 11:46:53 -0300

    Criação da classe constants

---
+++ src/app/app-constants.ts
@@ -0,0 +1,9 @@
+export class AppConstants {
+  public static SELECIONE: string = "Selecione";
+  public static CAMPO_OBRIGATORIO: string = "Campo obrigatório";
+
+  // Botões
+  public static BTN_SALVAR: string = "Salvar";
+  public static BTN_CANCELAR: string = "Cancelar";
+  public static BTN_VOLTAR: string = "Voltar";
+}
