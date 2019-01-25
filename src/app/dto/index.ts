341c92c4f31174f495970d16cd532cff740774d8
vandresandes <vandresandes@gmail.com>
2019-01-25 11:09:55 -0300

    Criação dos index

---
+++ src/app/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './checklist-pesquisa-dto';
+export * from './documento-dto';
+export * from './nucleo-dto';
+export * from './paginacao-dto';
+export * from './termo-especifico-dto';
+export * from './termo-geral-dto';
---
+++ src/app/enums/index.ts
@@ -0,0 +1,5 @@
+export * from './enum-crud.enum';
+export * from './enum-prioridade-tramitacao.enum';
+export * from './enum-prioridade.enum';
+export * from './enum-sigilo-segredo-justica.enum';
+export * from './enum-tipo-documento.enum';
